cba8932a29120f67f2d117f52b45316e9daa497c
Daniel Martin <danielwmartin1@gmail.com>
2024-03-03 18:50:19 -0500

    buttons added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import { Button } from "@mui/material";
 
 export default function Home() {
   return (
-    <h1>
-      Hello
-    </h1>
+    <div>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+    </div>
   );
 }
