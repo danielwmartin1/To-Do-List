a260896b6b5549491ba9e93e29cf83f320f9d1a6
Daniel Martin <danielwmartin1@gmail.com>
2024-03-09 14:27:41 -0500

    added script tags to html, exported default App in App.js, and imported Header into index.js

--- src/app/index.js
+++ src/app/index.js
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 ReactDOM.render(<App />, document.getElementById("root"));
