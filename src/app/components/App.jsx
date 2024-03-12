bea51954d1103aecceaf99755bf36828dd46ab60
Daniel Martin <danielwmartin1@gmail.com>
2024-03-12 01:37:17 -0400

    updated my index.js to use createRoot and added both header and footer react components to the App.js

--- src/app/components/App.jsx
+++ src/app/components/App.jsx
@@ -3,7 +3,9 @@ import React from "react";
 function App () {
   return (
     <div>
+      <Header />
       <h1>My To-Do-List</h1>
+      <Footer />
     </div>
   );
 }
--- src/app/index.js
+++ src/app/index.js
@@ -3,5 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-
-ReactDOM.render(<App />, document.getElementById("root"));
+// used concurrent mode with createRoot
+const root = createRoot(document.getElementById("root"));
+root.render(<App />, document.getElementById("root"));
