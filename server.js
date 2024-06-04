7181e05813c51b4cfd4bfa8514c3f2fbf049063d
Daniel Martin <danielwmartin1@gmail.com>
2024-06-04 01:42:03 -0400

    Update dependencies and server configuration, routing

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = 4000;
