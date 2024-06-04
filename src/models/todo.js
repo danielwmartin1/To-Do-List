ef80310a5884bcef69b6bb7fd0b59b9c758ee48b
Daniel Martin <danielwmartin1@gmail.com>
2024-06-04 10:18:31 -0400

    order key values for schema

--- src/models/todo.js
+++ src/models/todo.js
@@ -1,6 +1,10 @@
 import mongoose from 'mongoose';
 
 const todoSchema = new mongoose.Schema({
+    id: {
+        type: mongoose.Schema.Types.ObjectId,
+        default: mongoose.Types.ObjectId,
+    },
     title: {
         type: String,
         required: true,
@@ -9,10 +13,7 @@ const todoSchema = new mongoose.Schema({
         type: Boolean,
         default: false,
     },
-    id: {
-        type: mongoose.Schema.Types.ObjectId,
-        default: mongoose.Types.ObjectId,
-    },
+    
 });
 
 const Todo = mongoose.model('Todo', todoSchema);
