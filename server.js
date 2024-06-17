54613bfb05c1c83df115e1766620dddec10b1e1a
Daniel Martin <danielwmartin1@gmail.com>
2024-06-17 07:41:42 -0400

    fixed server.js routing

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
 })
 
 // Connect to MongoDB
-const BASE_URL = 'mongodb://localhost:27017';
+const BASE_URL = 'mongodb://localhost:27017/tasks';
 const mongodb = async () => {
   try {
     await mongoose.connect(BASE_URL);
