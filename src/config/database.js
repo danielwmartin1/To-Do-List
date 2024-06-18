13a7b7a6594e31fa01e6db4d517c11637f753479
Daniel Martin <danielwmartin1@gmail.com>
2024-06-18 08:53:34 -0400

    deleted unnecessary key: value pairs in the connectDB function

--- src/config/database.js
+++ src/config/database.js
@@ -3,10 +3,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/Tasks', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect('mongodb://localhost:27017/Tasks', {});
     console.log('MongoDB connected...');
   } catch (err) {
     console.error(err.message);
