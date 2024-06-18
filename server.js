29223b8ed7d32a0d59741fd4c03a5b5a4ac8e1e6
Daniel Martin <danielwmartin1@gmail.com>
2024-06-18 12:38:31 -0400

    error handling

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
+import connectDB from './src/config/database.js';
+//import Tasks from './src/models/Tasks.js';
 //import Task from './models/Tasks.js';
 //import tasks from './models/tasks.json';
 //import TaskRepository from './repositories/TaskRepository.js';
@@ -9,8 +11,6 @@ import mongoose from 'mongoose';
 //import TaskService from './services/TaskService.js';
 //import TaskRoutes from './routes/TaskRoutes';
 //import axios from 'axios';
-import connectDB from './src/config/database.js';
-import Tasks from './src/models/Tasks.js';
 
 
 const app = express();
@@ -44,45 +44,73 @@ connectDB();
 // In-memory data store
 
 app.get('/tasks', async (req, res) => {
-  res.send(tasks);
-  console.log('tasks', tasks);
+  try {
+    res.send(tasks);
+    console.log('tasks', tasks);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server Error');
+  }
 });
 
 app.get('/tasks/:id', async (req, res) => {
-  const id = parseInt(req.params.id);
-  const task = tasks.find((task) => task.id === id);
-  res.send(task);
-  console.log('task', task);
+  try {
+    const id = parseInt(req.params.id);
+    const task = tasks.find((task) => task.id === id);
+    if (!task) {
+      return res.status(404).send('Task not found');
+    }
+    res.send(task);
+    console.log('task', task);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server Error');
+  }
 });
 
 app.post('/tasks', async (req, res) => {
-  const newTask = { 
-    id: tasks.length + 1, 
-    title: req.body.title,
-  };
-  tasks.push(newTask);
-  res.send(tasks);
-  console.log('Posted a task titled', req.body.title);
+  try {
+    const newTask = { 
+      id: tasks.length + 1, 
+      title: req.body.title,
+    };
+    tasks.push(newTask);
+    res.send(tasks);
+    console.log('Posted a task titled', req.body.title);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server Error');
+  }
 });
 
 app.put('/tasks/:id', async (req, res) => {
-  const id = parseInt(req.params.id);
-  const updatedTaskData = req.body;
-  tasks = tasks.map((task) => {
-    if (task.id === id) {
-      console.log('Edited to', updatedTaskData); // Move logging inside the condition
-      return { ...task, ...updatedTaskData }; // Correctly merge updated data with the existing task
-    }
-    return task; // Return the task as is if the id doesn't match
-  });
-  res.send(tasks); // Send back the updated tasks array as a response
+  try {
+    const id = parseInt(req.params.id);
+    const updatedTaskData = req.body;
+    tasks = tasks.map((task) => {
+      if (task.id === id) {
+        console.log('Edited to', updatedTaskData); // Move logging inside the condition
+        return { ...task, ...updatedTaskData }; // Correctly merge updated data with the existing task
+      }
+      return task; // Return the task as is if the id doesn't match
+    });
+    res.send(tasks); // Send back the updated tasks array as a response
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server Error');
+  }
 });
 
 app.delete('/tasks/:id', async (req, res) => { 
-  const id = parseInt(req.params.id);
-  tasks = tasks.filter((task) => task.id !== id);
-  res.send(tasks);
-  console.log('Deleted id' + id);
+  try {
+    const id = parseInt(req.params.id);
+    tasks = tasks.filter((task) => task.id !== id);
+    res.send(tasks);
+    console.log('Deleted id' + id);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server Error');
+  }
 });
 
 
