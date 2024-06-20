d1d32ae598b93067ab3c6107ef0bcb6605259ecb
Daniel Martin <danielwmartin1@gmail.com>
2024-06-20 09:32:09 -0400

    commented out the completed key:value pair in Tasks.js

--- src/models/Tasks.js
+++ src/models/Tasks.js
@@ -5,11 +5,11 @@ const TaskSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  completed: {
-    type: Boolean,
-    default: false,
+  //completed: {
+    //type: Boolean,
+    //default: false,
     //absent in the frontend
-  },
+  //},
 });
 
 const Tasks = mongoose.model('Tasks', TaskSchema);
