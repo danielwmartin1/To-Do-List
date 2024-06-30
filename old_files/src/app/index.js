4428f9c3a4709ed5694eb8cf532f124713a91b69
Daniel Martin <danielwmartin1@gmail.com>
2024-06-30 10:59:45 -0400

    remove old files

--- old_files/src/app/index.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import App from "./components/App";
-// used concurrent mode with createRoot
-const root = createRoot(document.getElementById("root"));
-root.render(<React.StrictMode><App /></React.StrictMode>, document.getElementById("root"));
