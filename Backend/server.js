d6b7ddd72fcb93fab520d036fe2f8e5f0b0f51cf
Daniel Martin <danielwmartin1@gmail.com>
2024-07-14 16:35:56 -0400

    separated backend and frontend

--- Backend/server.js
+++ Backend/server.js
@@ -3,7 +3,7 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
-import TaskRepository from './Backend/repositories/TaskRepository.js';
+import TaskRepository from './repositories/TaskRepository.js';
 
 // Initialize Express application
 const app = express();
