332dfdfe7201a47549755cd543b8c66826e390d8
danielwmartin1 <danielwmartin1@gmail.com>
2024-09-03 05:57:36 -0400

    refactor: Update footer position to fixed and adjust font size for better mobile responsiveness

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -4,6 +4,8 @@ import "../index.css";
 function Footer() {
   // Get the current year
   const year = new Date().getFullYear();
+  //const month = new Date().toLocaleString('default', { month: 'long' });
+  //const day = new Date().getDate();
   return (
     <React.StrictMode>
       <div className="footer">
